87c1595e0adba40029d23822c4dae365458dac46
Emanuele <emazanni2022@gmail.com>
2026-04-10 13:13:59 -0300

    Mergeado correctamente

--- Integrantes/menus.py
+++ Integrantes/menus.py
@@ -4,33 +4,8 @@ from Integrantes.roles import*
 
 
 
-<<<<<<< HEAD
 def imprimirMenuIntegrantes(ListaIntegrantes,ListaRoles):
-    clearConsole()
-    print("[Menu Principal > *Integrantes*]")
-    print()
-    print("1. Ver integrantes")
-    print("2. Agregar integrante")
-    print("3. Editar integrante")
-    print("4. Eliminar integrante")
-    print("5. Gestionar roles")
-    opcion=input("Seleccione una opcion: ")
-    if opcion=="1":
-        ver_integrantes(ListaIntegrantes)
-    elif opcion=="2":
-        agregar_integrante(ListaIntegrantes)
-    elif opcion=="3":
-        editar_integrante(ListaIntegrantes)
-    elif opcion=="4":
-        eliminar_integrante(ListaIntegrantes)
-    elif opcion=="5":
-        imprimirMenuRoles(ListaRoles)
-    else:
-        print("Opcion invalida. Intente nuevamente.")
-=======
-def imprimirMenuIntegrantes(ListaIntegrantes):
     activo=True
-    
     while activo:
         clearConsole()
         print("[Menu Principal > *Integrantes*]")
@@ -39,6 +14,7 @@ def imprimirMenuIntegrantes(ListaIntegrantes):
         print("2. Agregar integrante")
         print("3. Editar integrante")
         print("4. Eliminar integrante")
+        print("5. Gestionar roles")
         print("0. Volver atras")
         opcion=input("Seleccione una opcion: ")
         if opcion=="1":
@@ -49,8 +25,9 @@ def imprimirMenuIntegrantes(ListaIntegrantes):
             editar_integrante(ListaIntegrantes)
         elif opcion=="4":
             eliminar_integrante(ListaIntegrantes)
-        elif opcion=="0":
-           activo=False 
+        elif opcion=="5":
+            imprimirMenuRoles(ListaRoles)
+        elif opcion == "0":
+            activo=False
         else:
             print("Opcion invalida. Intente nuevamente.")
->>>>>>> 1abf464305aacaba8085f5ba1e5d4bab5d0e6f1e
